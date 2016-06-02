c508a2ac9aa87c14869a4088828e8517530be7e0
v.kislyak <v.kislyak@smissltd.com>
2016-06-02 16:55:31 +0300

    single task working

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -27,7 +27,7 @@ angular.module('tt')
       }
     };
 
-    $scope.clicked = function (task) {
+    $scope.clicked = function (task) {// переход в конкретную задачу
       $state.go('app.order');
       dataService.currentTask = task;
       console.log(dataService.currentTask)
@@ -68,44 +68,64 @@ angular.module('tt')
     }
   }/*]*/)
   .controller('OrderCtrl', function ($scope, $stateParams, taskToggle, taskService, dataService, $interval, $filter) {
+    $scope.currentTask = dataService.currentTask;
+
     $scope.Timer = null;
     $scope.StartTimer = function (num) {
       //Set the Timer start message.
-      $scope.showTime = "Timer started. ";
+       $scope.showTime = "Timer started. ";
+      
 
       //Initialize the Timer to run every 1000 milliseconds i.e. one second.
       $scope.Timer = $interval(function () {
+        var h = Math.floor(num / 3600);
+        var m = Math.floor(num / 60) % 60;
+        var s = num % 60;
+        if (m < 10)
+          m = '0' + m;
+        if (s < 10)
+          s = '0' + s;
         //Display the current time.
-        var time = $filter('date')(num, 'HH:mm:ss');
+        $scope.currentTask.time++;
+        num++;
+        var time = h+':'+m+':'+s//$filter('date')(num, 'HH:mm:ss');
         $scope.showTime = time;
+        
       }, 1000);
+
     };
     $scope.StopTimer = function (num) {
       //Set the Timer stop showTime.
       var time = $filter('date')(num, 'HH:mm:ss');
-      $scope.showTime = time;
+      $scope.showTime = 'Timer stopped';
       //Cancel the Timer.
       if (angular.isDefined($scope.Timer)) {
         $interval.cancel($scope.Timer);
       }
+      console.log($scope.showTime)
     };
-    $scope.currentTask = dataService.currentTask;
+
+    if($scope.currentTask.current===true){
+      $scope.StartTimer($scope.currentTask.time)
+    }
     $scope.toggleT = function (task) {
       taskToggle.toggleState(task.id)
         .then(function success(resp) {
           console.log(resp);
-          if (!resp.data.started) {
+          if (resp.data.started===false && resp.data.success===true) {
 
             // start timer
+            $scope.currentTask.current = false;
             $scope.StopTimer();
-            // dataService.tasksList = $scope.tasksList;
-            // updateTaskTime(task);
+            dataService.currentTask = $scope.currentTask;
+
             console.log(resp.data.started);
-          } else {
+          } else if(resp.data.started===true && resp.data.success===true){
             // stop timer
             $scope.StartTimer(task.time);
-            // dataService.tasksList = $scope.tasksList;
-            // setInterval(updateTaskTime(task), 500);
+            $scope.currentTask.current = true;
+
+            dataService.currentTask = $scope.currentTask;
             console.log(resp.data.started);
           }
           taskService.requestData()
