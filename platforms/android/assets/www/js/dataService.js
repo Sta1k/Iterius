430efa45009044aca6987af9a37d090cb006a22b
v.kislyak <v.kislyak@smissltd.com>
2016-06-09 13:20:30 +0300

    fixes in design and bug with header timer

--- platforms/android/assets/www/js/dataService.js
+++ platforms/android/assets/www/js/dataService.js
@@ -4,10 +4,9 @@
 APP
   .service('dataService', function () {
     this.tasksList = {};
-    currentTask = {
-      current: null,
-      name: 'stringName'
-    };
+    this.currentUser=null;
+    this.currentTask = {};
+    this.memberTasks=[];
     this.AllWorkedTime = 0;
     this.showTime = undefined;
   });
@@ -36,6 +35,31 @@ APP
       }
     }
   });
+APP
+  .factory('updateMemberTime', function ($interval, dataService) {//$scope нельзя передавать в сервисы
+    return {
+      Timer: null,
+      StartTimer: function (task) {
+        var obj = _.findWhere(dataService.memberTasks, {id: task.id});
+        this.Timer = $interval(function () {
+          obj.time++;
+          // console.log(obj.time);
+          var arr = _.pluck(dataService.memberTasks, 'time');
+          summa = function (m) {
+            for (var s = 0, k = m.length; k; s += m[--k]);
+            dataService.AllWorkedTime = s;
+          };
+          summa(arr);
+        }, 1000);
+      },
+      StopTimer: function (task) {
+        if (angular.isDefined(this.Timer)) {
+          $interval.cancel(this.Timer);
+        }
+      }
+    }
+  });
+
  APP 
    .factory('showTeam',function () {
      return{
