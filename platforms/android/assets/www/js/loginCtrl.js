9bd838446400c34ec58cf193af636e05c72f904b
v.kislyak <v.kislyak@smissltd.com>
2016-07-05 16:48:32 +0300

    added Remember Me functionality

--- platforms/android/assets/www/js/loginCtrl.js
+++ platforms/android/assets/www/js/loginCtrl.js
@@ -1,45 +1,51 @@
 APP
   .controller('LoginCtrl',
-     function ($scope,
-               $ionicPlatform,
-               $cordovaTouchID,
-               data,
-               APIService ,
-               $state,
-               dataService,
-               $stateParams,
-               $timeout) {
-  ionic.Platform.ready(function(){
-     $scope.isAndroid = ionic.Platform.isAndroid();
-     $scope.user = {};
-     
-  $timeout(dataService.checkRemember()).then(function(){
-         $scope.check=data.check
-         if ($scope.check == 'on'){
-          $scope.LogIn(data.user)
+    function ($scope,
+              $ionicPlatform,
+              $cordovaTouchID,
+              data,
+              APIService,
+              $state,
+              dataService,
+              $stateParams,
+              $timeout) {
+      ionic.Platform.ready(function () {
+        $scope.isAndroid = ionic.Platform.isAndroid();
+        $scope.user = {};
+
+        dataService.checkRemember();//.then(function (res) {
+          $timeout(remember,1000);
+                
+
+      });
+
+      function remember() {
+        $scope.check = data.check;
+        console.log($scope.check);
+        if ($scope.check == 'on') {
+          //$scope.LogIn(data.user)
+          $scope.user = data.user
         }
-      })
-    
-  })
-         
 
+      }
+      
       $scope.LogIn = function (objUser) {
-        
         APIService.login(objUser)
-
           .then(function
             success(response) {
             console.log(response);
             dataService.login = objUser;
 
             if (response.data.success) {
-              if($scope.user.remember==true&&$scope.user.password.length>6){
+              if ($scope.user.remember == true && $scope.user.password.length > 6) {
                 dataService.rememberMe($scope.user)
               }
-                    $state.go('app.tasks');
-                  
+              if($scope.user.remember == false){
+                dataService.delRemember()
+              }
+              $state.go('app.tasks');
 
-                
+
             } else {
 
               alert(response.data.errors.password[0]);
@@ -50,47 +56,27 @@ APP
             console.log(res.status + ' ' + res.statusText);
           });
 
-      }
-      
-    $scope.StartTouch = function(){
-       $cordovaTouchID.checkSupport().then(function() {
-    $cordovaTouchID.authenticate("You must authenticate").then(function() {
-      $timeout(dataService.readDb()).then(function() {
-        $scope.LogIn(data.user)
-      },function (error) {
-        alert('error DB')
-      })
-    // success
-  }, function () {
-    // error
-  });
-  }, function (error) {
-    alert(error); // TouchID not supported
-  });
-      // success(//function() {
-    
-      //   $cordovaTouchID.authenticate("You must authenticate")
-      //   .success(dataService.readDb)
-                                         
-      //   .success(APIService.login(data.user))
-      //    .success(function (response) {
-      //       console.log(response);
-      //       dataService.login = data.user;
-            
+      };
 
-      //       if (response.data.success) {
-              
-      //               $state.go('app.tasks');                
-      //       } else {
-      //         alert(response.data.errors.password[0]);
-      //       }
+      $scope.StartTouch = function () {
+        $cordovaTouchID.checkSupport().then(function () {
+          $cordovaTouchID.authenticate("You must authenticate").then(function () {
+            $timeout(dataService.readDb()).then(function () {
+              $scope.LogIn(data.user)
+            }, function (error) {
+              alert('error DB')
+            });
+            // success
+          }, function () {
+            // error
+          });
+        }, function (error) {
+          alert(error); // TouchID not supported
+        });
 
-      //     }, function err(res) {
-      //       console.log(res.status + ' ' + res.statusText);
-      //     }))
-      }//end function 
+      };//end function
 
-               })
+    });
 
-  
+
 
--- platforms/android/assets/www/js/services.js
+++ platforms/android/assets/www/js/services.js
@@ -2,7 +2,7 @@ APP
   .service('APIService', function ($http, $httpParamSerializerJQLike) {
     var url = 'http://dev.tt.smiss.ua/api/';
     this.login = function (request) {//loginService.sendData to APIService.login
-      console.log(request);
+      // console.log(request);
       var login = {
         model: {
           username: request.username,
